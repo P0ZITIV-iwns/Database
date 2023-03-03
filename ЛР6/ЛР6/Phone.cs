03a7b4214ac86c084817ca99537f5e94fe8df6df
P0ZITIV <66378765+P0ZITIV-iwns@users.noreply.github.com>
2023-03-03 20:58:57 +0500

    Update Phone.cs

--- ЛР6/ЛР6/Phone.cs
+++ ЛР6/ЛР6/Phone.cs
@@ -1,15 +1,18 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static ЛР6.MainWindow;
 
 namespace ЛР6
 {
-    internal class Phone
+    public class Phone
     {
+        public int Id { get; set; }
         public string Title { get; set; }
-        public string Company { get; set; }
+        public int CompanyId { get; set; }
         public decimal Price { get; set; }
+        public Company CompanyEntity { get; set; }
     }
 }
