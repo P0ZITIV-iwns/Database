b72b1427502865d5e9eea1307b712b17caca7c88
P0ZITIV <66378765+P0ZITIV-iwns@users.noreply.github.com>
2023-03-30 18:29:02 +0500

    Add files via upload

---
+++ ЛР9/ЛР9/AboutPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ЛР9
+{
+    /// <summary>
+    /// Логика взаимодействия для AboutPage.xaml
+    /// </summary>
+    public partial class AboutPage : Page
+    {
+        public AboutPage(Phone _phone)
+        {
+            InitializeComponent();
+
+            titlePageView.Text = _phone.title;
+            definitionPageView.Text = _phone.definition;
+            companyPageView.Text = _phone.CompanyEntity.title;
+            pricePageView.Text = _phone.price.ToString();
+            BitmapImage _bitmapImage = new BitmapImage();
+            using (Stream stream = File.OpenRead(_phone.imagesource))
+            {
+                _bitmapImage.BeginInit();
+                _bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                _bitmapImage.StreamSource = stream;
+                _bitmapImage.EndInit();
+            }
+            imagePageView.Source = _bitmapImage;
+        }
+
+        private void BackArrowButton_Click(object sender, RoutedEventArgs e)
+        {
+            FrameContext.MainWindowFrame.Navigate(null);
+        }
+
+    }
+}
---
+++ ЛР9/ЛР9/DatabaseControl.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЛР9
+{
+    public static class DatabaseControl
+    {
+        public static List<Company> GetCompanies()
+        {
+            using (DbAppContext ctx = new DbAppContext())
+            {
+                return ctx.company.Include(p => p.PhoneEntities).ToList();
+            }
+        }
+        public static List<Phone> GetPhonesForView()
+        {
+            using (DbAppContext ctx = new DbAppContext())
+            {
+                return ctx.phones.Include(p => p.CompanyEntity).ToList();
+            }
+        }
+        public static void AddPhone(Phone phone)
+        {
+            using (DbAppContext ctx = new DbAppContext())
+            {
+                ctx.phones.Add(phone);
+                ctx.SaveChanges();
+            }
+        }
+        public static void UpdatePhone(Phone phone)
+        {
+            using (DbAppContext ctx = new DbAppContext())
+            {
+                Phone _phone = ctx.phones.FirstOrDefault(p => p.id == phone.id);
+
+                if (_phone == null)
+                {
+                    return;
+                }
+
+                _phone.title = phone.title;
+                _phone.price = phone.price;
+                _phone.companyid = phone.companyid;
+                _phone.definition = phone.definition;
+                _phone.imagesource = phone.imagesource;
+
+                ctx.SaveChanges();
+            }
+        }
+        public static void RemovePhone(Phone phone)
+        {
+            using (DbAppContext ctx = new DbAppContext())
+            {
+                ctx.phones.Remove(phone);
+                ctx.SaveChanges();
+            }
+            if (phone.imagesource != "D:\\PhonePictures\\noImage.png")
+            {
+                File.Delete(phone.imagesource);
+            }
+        }
+    }
+}
